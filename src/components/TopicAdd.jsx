78ae7caf80f80e44dcadb14f839e17e76450055e
randotm <randotm@users.noreply.github.com>
2017-11-24 12:24:21 +0200

    No errors on page for the moment.

--- src/components/TopicAdd.jsx
+++ src/components/TopicAdd.jsx
@@ -5,32 +5,19 @@ import Breadcrumbs from './Breadcrumbs'
 import TableWrap from '../components/TableWrap'
 import getTabs from '../utils/getTabs'
 
-const { func, object, shape, string } = PropTypes
+const { array, bool, func, shape } = PropTypes
 
-const propTypes = {
-  clearTableContent: func.isRequired,
-  curriculum: object,
-  defaultTab: string,
-  getTableContent: func.isRequired,
-  history: shape({
-    replace: func.isRequired,
-    location: shape({
-      pathname: string.isRequired,
-      search: string.isRequired
-    }).isRequired
-  }).isRequired,
-  queryExtend: object,
-  search: object,
-  supervisor: object,
-  tableContent: object.isRequired,
-  tabs: object.isRequired
-}
-/*
-render () {
-	return ()
-}
-*/
+const propTypes = {}
+
 class TopicAdd extends React.Component {
+
+  render () {
+    return (
+      <div className='topicadd'>
+        <h1>Lõputöö teema registreerimise vorm</h1>
+      </div>
+    )
+  }
 
 }
 
