aa18ad624d849309589e01195d81da1fcda1626d
rasmusaaviste <rasmusaaviste@users.noreply.github.com>
2017-12-11 13:57:16 +0200

    [15] Every page has its own name now with suffix "Teemaderegister" (#92)
    * [15] Every page has its own name now with suffix "Teemaderegister"
    
    * Put setTitle from componentWillUnmount to componentDidMount
    
    * renamed setTitle to setDocTitle, added minor fixes for helper
    
    * [15] Code review changes, moved page title changing to TableWrap
    
    * minor fixes and modified creating title string
    
    * better explanation
    
    * Added document titles to password resetting pages
    
    * Show simple Search when no q

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -2,3 +2,11 @@ export const removeEmpty = obj => {
   Object.keys(obj).forEach(key => obj[key] == null && delete obj[key])
   return obj
 }
+
+export const setDocTitle = title => {
+  document.title = `${title} | Teemaderegister`
+}
+
+export const capitalizeFirstLetter = string => {
+  return string[0].toUpperCase() + string.slice(1)
+}
