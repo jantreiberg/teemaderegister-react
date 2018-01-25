b81c6ac86e290844913b2bd12310c70793de99c5
Unknown <vaskpaul@gmail.com>
2018-01-25 14:34:23 +0200

    added initial changes

--- src/components/AdminUsers.jsx
+++ src/components/AdminUsers.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
-
 const propTypes = {
   type: PropTypes.string
 }
+
+const supervisors = [{
+  key: '1',
+  email: 'john@doe.ee',
+  firstName: 'Mike',
+  lastName: 'Dude'
+}]
+/*
+const students = [{
+  key: '1',
+  email: 'john@doe.ee',
+  firstName: 'Mike',
+  lastName: 'Dude'
+}]
+*/
+const columns = [{
+  title: 'firstName',
+  dataIndex: 'firstName',
+  key: 'firstName'
+}, {
+  title: 'lastName',
+  dataIndex: 'lastName',
+  key: 'lastName'
+}, {
+  title: 'email',
+  dataIndex: 'email',
+  key: 'email'
+}]
 
 const AdminUsers = props => {
   switch (props.type) {
     case 'supervisor':
       return (
-        <h2>Registered supervisors</h2>
+        <table supervisors={supervisors} columns={columns} />
       )
     case 'students':
       return (
