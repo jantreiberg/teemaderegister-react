3e7fbf68c49d00c88bc84664ebc6897fe99e79af
vaskpaul <vaskpaul@gmail.com>
2018-02-26 14:06:59 +0200

    Admin can now see supervisors

--- src/components/AdminUsers.jsx
+++ src/components/AdminUsers.jsx
@@ -1,50 +1,77 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
+import { Table } from 'antd'
+import Api from '../utils/Api'
+import debounce from 'lodash.debounce'
+import { SUPERVISORS_URL } from '../constants/ApiConstants'
+
 const propTypes = {
   type: PropTypes.string
 }
 
-const supervisors = [{
-  key: '1',
-  email: 'john@doe.ee',
-  firstName: 'Mike',
-  lastName: 'Dude'
-}]
-/*
-const students = [{
-  key: '1',
-  email: 'john@doe.ee',
-  firstName: 'Mike',
-  lastName: 'Dude'
-}]
-*/
-const columns = [{
-  title: 'firstName',
-  dataIndex: 'firstName',
-  key: 'firstName'
-}, {
-  title: 'lastName',
-  dataIndex: 'lastName',
-  key: 'lastName'
-}, {
-  title: 'email',
-  dataIndex: 'email',
-  key: 'email'
-}]
+class AdminUsers extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      supList: [],
+      gotSupervisors: false
+    }
+    this.supervisors = this.state.supList
+    this.getSupervisors = debounce(this.getSupervisors.bind(this), 500)
+  }
 
-const AdminUsers = props => {
-  switch (props.type) {
-    case 'supervisor':
-      return (
-        <table supervisors={supervisors} columns={columns} />
-      )
-    case 'students':
-      return (
-        <h2>Registered students</h2>
-      )
-    default:
-      return (<h2>Choose something.</h2>
-      )
+  getSupervisors () {
+    if (this.state.gotSupervisors === true) return
+    // this.setState(() => {
+    Api('GET', SUPERVISORS_URL)
+      .then(body => {
+        const supList = body.supervisors.map(user => ({
+          email: user.slug,
+          key: user._id,
+          firstName: user.supervisor,
+          lastName: 'test',
+          value: user._id
+        }))
+        this.setState({ supList, gotSupervisors: true })
+      })
+  }
+  render () {
+    this.getSupervisors()
+    const columns = [{
+      title: 'First name',
+      dataIndex: 'firstName',
+      key: 'firstName'
+    }, {
+      title: 'Last name',
+      dataIndex: 'lastName',
+      key: 'lastName'
+    }, {
+      title: 'E-mail',
+      dataIndex: 'email',
+      key: 'email'
+    }]
+
+    const students = []
+
+    switch (this.props.type) {
+      case 'supervisor':
+        return (
+          <div>
+            <h2>Supervisors</h2>
+            <Table dataSource={this.state.supList} columns={columns}/>
+          </div>
+        )
+      case 'students':
+        return (
+          <div>
+            <h2>Registered students</h2>
+            <Table dataSource={students} columns={columns}/>
+          </div>
+        )
+      default:
+        return (<h2>Choose something.</h2>
+        )
+    }
   }
 }
 
